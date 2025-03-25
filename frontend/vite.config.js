92f2ec943a21a289215596803b9f0bb5b26c5f53
duncorir <duncorir@mail.com>
2025-03-25 17:42:51 +0300

    home page

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,9 +4,12 @@ import react from '@vitejs/plugin-react';
 // Replace 'your-username' and 'Gaine_Africa_app' with your actual GitHub username and repo name
 export default defineConfig({
   plugins: [react()],
-  base: "/", // Ensures assets are correctly loaded on GitHub Pages
+  base: '/Gaine_Africa_app/', // Ensures assets are correctly loaded on GitHub Pages
   server: {
     port: 5173,   // Keeps your local setup
     open: true
   },
+  build: {
+    outDir: 'dist', // Ensures the build is placed in the right folder
+  }
 });
